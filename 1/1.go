0f10942d0afa10309a2bd5391ca42613521fe18e
Jordan Raffoul <me@jordanraffoul.com>
2024-12-02 20:55:10 -0500

    day 1 part 2

--- 1/1.go
+++ 1/1.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 )
 
-func main() {
+func p1() int {
 	l1 := make([]int, 0)
 	l2 := make([]int, 0)
 
@@ -39,7 +39,7 @@ func main() {
 	slices.Sort(l1)
 	slices.Sort(l2)
 
-	differences := 0
+	diffs := 0
 	for i := range l1 {
 		i1 := l1[i]
 		i2 := l2[i]
@@ -48,9 +48,54 @@ func main() {
 		if d < 0 {
 			d = d * -1
 		}
-		differences += d
-		//fmt.Println(i1, i2, d)
+		diffs += d
 	}
 
-	fmt.Println(differences)
+	return diffs
+}
+
+func p2() int {
+	l1 := make([]int, 0)
+	s := make(map[int]int)
+	ss := 0
+
+	file, err := os.Open("input")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		split := strings.Split(line, "   ")
+
+		s1 := split[0]
+		s2 := split[1]
+
+		i1, _ := strconv.Atoi(s1)
+		i2, _ := strconv.Atoi(s2)
+
+		// Append to left list
+		l1 = append(l1, i1)
+		// Build right map
+		if _, ok := s[i2]; ok {
+			s[i2] = s[i2] + 1
+		} else {
+			s[i2] = 1
+		}
+	}
+
+	for _, val := range l1 {
+		if _, ok := s[val]; ok {
+			ss += val * s[val]
+		}
+	}
+
+	return ss
+}
+
+func main() {
+	fmt.Println(p1())
+	fmt.Println(p2())
 }
